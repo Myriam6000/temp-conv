bac8aab15ec1290ab073e0855b6c424eb96fbc71
Myriam6000 <myriam.forgeron@gmail.com>
2019-05-27 21:38:18 +0200

    added new files

---
+++ converter.js
@@ -0,0 +1,8 @@
+
+//Exercice Celsius - Fahrenheit converter in JS
+
+function converter(celsius) {
+    return (celsius * 9/5) + 32;
+    }
+
+console.log ("15 degrees celsius is equal to " + converter(15) + " degrees Fahrenheit");
